62a7bfec4217a192823dea94269835e371d23ed1
Marcos Antonio Avila Morales <mis.redes.work@gmail.com>
2025-01-03 02:44:39 -0500

    ⏫ `Update Footer and Hero components to remove redundant path in download links`

--- src/components/seccions/Footer.tsx
+++ src/components/seccions/Footer.tsx
@@ -4,14 +4,14 @@ import { Button } from '../ui/button';
 
 const Footer = () => {
   return (
-    <footer className="bg-muted w-full text-muted-foreground py-8 px-4 flex justify-center transition-colors duration-300">
+    <footer className="flex justify-center bg-muted px-4 py-8 w-full text-muted-foreground transition-colors duration-300">
       <div className="flex flex-col justify-between items-center">
-        <a href="/src/assets/Marcos_Avila_CV.pdf" download>
-          <Button variant="outline" className="mb-4 text-secondary-foreground border-muted-foreground bg-secondary hover:bg-secondary/10 hover:border-ring transition-colors duration-300">
+        <a href="Marcos_Avila_CV.pdf" download>
+          <Button variant="outline" className="border-muted-foreground bg-secondary hover:bg-secondary/10 mb-4 hover:border-ring text-secondary-foreground transition-colors duration-300">
             <ArrowDownToLine />Download CV
           </Button>
         </a>
-        <p className="text-sm text-muted-foreground transition-colors duration-300">&copy; 2025 Marcos Avila. All rights reserved.</p>
+        <p className="text-muted-foreground text-sm transition-colors duration-300">&copy; 2025 Marcos Avila. All rights reserved.</p>
       </div>
     </footer>
   );
